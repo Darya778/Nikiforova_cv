0b795b7ac79970aaded29bf72a1d3f9d6994c7ab
Darya778 <112837727+Darya778@users.noreply.github.com>
2024-12-27 16:54:54 +0800

    Update main.py

--- pictures/main.py
+++ pictures/main.py
@@ -5,8 +5,7 @@ c = 0
 
 ret, frame = capture.read()
 while frame is not None:
-    blurred = cv2.GaussianBlur(frame, (9, 9), 0)
-    gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(gray, 60, 255, cv2.THRESH_BINARY)
 
     contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
